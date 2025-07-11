7cc3a9d9526b1083119fac2b844e2b189d2f1058
Afomiya <afomiyaketsella12@gmail.com>
2025-07-11 19:30:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
       const newLevels = { ...skillLevels };
       delete newLevels[skill];
       setSkillLevels(newLevels);
-    } else if (selectedSkills.length < 10) { // Limit to
+    } else if (selectedSkills.length < 10) { // Limi
       setSelectedSkills([...selectedSkills, skill]);
     }
   };
