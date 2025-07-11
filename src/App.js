5be1d6e91208d557d92970873a6e14c3eaced47f
Afomiya <afomiyaketsella12@gmail.com>
2025-07-11 19:28:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   const [darkMode, setDarkMode] = useState(false);
   const [showAssessmentModal, setShowAssessmentModal] = useState(false);
 
-  // Define handleSkillSelect at the
+  // Define handleSkillSelect
   const handleSkillSelect = (skill) => {
     if (selectedSkills.includes(skill)) {
       setSelectedSkills(selectedSkills.filter(s => s !== skill));
