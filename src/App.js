b9a410cc4d230da67d5f2d7a976ff0755d57c1fb
Afomiya <afomiyaketsella12@gmail.com>
2025-07-15 21:34:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -341,7 +341,7 @@ function App() {
     const partialMatches = [];
 
     jobData.forEach(job => {
-      // Skip if job has specific location and 
+      // Skip if job has specific location
       if (job.location && job.location !== 'Remote' && location !== 'Global' && job.location !== location) {
         return;
       }
