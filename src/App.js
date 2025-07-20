4e3f38ac725ac72fdb31b08a8f81c4671247a9c1
Afomiya <afomiyaketsella12@gmail.com>
2025-07-20 19:38:32 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -376,7 +376,7 @@ function App() {
 
   const { perfectMatches, strongMatches, partialMatches } = calculateMatches();
 
-  // Helper functions
+  // Helper
   const calculateDemandScore = () => {
     const totalJobs = jobData.length;
     const matchingJobs = jobData.filter(job => 
